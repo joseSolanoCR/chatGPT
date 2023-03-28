17ff682edc520402af7b90a62639c8f90347db1d
josesolano <jsolanoca@gmail.com>
2023-03-28 10:53:14 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+import openai
+from transformers import pipeline
+
+OPENAI_API_KEY = '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'
+org_id = 'org-N4mFsDIcRyohiqOCUpvKUHRy'
+
+
+openai.organization = org_id
+openai.api_key = OPENAI_API_KEY
+
+
+
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B', tokenizer='EleutherAI/gpt-neo-2.7B')
+
+def generate_response(prompt):
+    response = generator(prompt, max_length=1024, do_sample=True, temperature=0.7)
+
+    message = response[0]['generated_text'].strip()
+    return message
+
+print(generate_response("Hola, ¿cómo estás?"))
